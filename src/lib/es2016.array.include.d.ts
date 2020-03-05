e20990a2304e3edbd7a6152f1ad34eca93e16a94
Jason Yu <jason.yu@askattest.com>
2020-03-05 09:25:41 +0000

    allow includes to take argument of type that is a superset of the array type

--- src/lib/es2016.array.include.d.ts
+++ src/lib/es2016.array.include.d.ts
@@ -4,7 +4,10 @@ interface Array<T> {
      * @param searchElement The element to search for.
      * @param fromIndex The position in this array at which to begin searching for searchElement.
      */
-    includes(searchElement: T, fromIndex?: number): boolean;
+    includes<U>(
+        searchElement: U,
+        fromIndex?: number
+    ): searchElement is T extends U ? T : U;
 }
 
 interface ReadonlyArray<T> {
@@ -13,7 +16,10 @@ interface ReadonlyArray<T> {
      * @param searchElement The element to search for.
      * @param fromIndex The position in this array at which to begin searching for searchElement.
      */
-    includes(searchElement: T, fromIndex?: number): boolean;
+    includes<U>(
+      searchElement: U,
+      fromIndex?: number
+    ): searchElement is T extends U ? T : U;
 }
 
 interface Int8Array {
